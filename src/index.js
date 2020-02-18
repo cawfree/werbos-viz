278dad081d34fe4db0fd90db9eeb92f7829f0dc4
cawfree <hello@cawfree.com>
2020-02-18 13:52:40 +0000

    Prepared dist/directory for external integration testing.

--- src/index.js
+++ src/index.js
@@ -6,95 +6,102 @@ import sabrina from "sabrina";
 import open from "open";
 import { typeCheck } from "type-check";
 
-const defaultOptions = Object.freeze(
-  {
-    title: undefined,
-  },
-);
-
-// TODO: Likely a function of config.
-const url = 'http://localhost:3000';
-
-const request = (title, children) => axios({
-  headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
-  url: `${url}/pane`,
-  method: 'post',
-  data: {
-    title,
-    children,
-  },
+const defaultOptions = Object.freeze({
+  title: undefined
 });
 
-const requestLine = (title, data) => request(
-  title,
-  {
-    _: 'Line',
+// TODO: Likely a function of config.
+const url = "http://localhost:3000";
+
+const request = (title, children) =>
+  axios({
+    headers: { "Content-Type": "application/json", Accept: "application/json" },
+    url: `${url}/pane`,
+    method: "post",
     data: {
-      labels: [...Array(Math.max(...Object.values(data).map(({ data: { length } }) => length)))]
-        .map((_, i) => `${i}`),
-      datasets: Object.entries(data)
-        .map(
-          ([label, { data, backgroundColor }]) => ({
-            label,
-            data,
-            backgroundColor,
-          }),
-        ),
-    },
-  },
-);
+      title,
+      children
+    }
+  });
 
-const requestJson = (title, data) => request(
-  title,
-  {
-    _: 'Json',
-    data,
-  },
-);
+const requestLine = (title, data) =>
+  request(title, {
+    _: "Line",
+    data: {
+      labels: [
+        ...Array(
+          Math.max(...Object.values(data).map(({ data: { length } }) => length))
+        )
+      ].map((_, i) => `${i}`),
+      datasets: Object.entries(data).map(
+        ([label, { data, backgroundColor }]) => ({
+          label,
+          data,
+          backgroundColor
+        })
+      )
+    }
+  });
 
-const ensureServerLoaded = () => axios({ method: 'get', url })
-  .catch(
-    e => sabrina(
-      {
-        "react-chartjs-2": ["Line"],
-        "react-json-tree": [["default", "Json"]],
-      },
-      { title: '🧠 werbos' },
+const requestJson = (title, data) =>
+  request(title, {
+    _: "Json",
+    data
+  });
+
+const ensureServerLoaded = () =>
+  axios({ method: "get", url })
+    .catch(e =>
+      sabrina(
+        {
+          "react-chartjs-2": ["Line"],
+          "react-json-tree": [["default", "Json"]]
+        },
+        { title: "🧠 werbos" }
+      ).then(() => open(url))
     )
-    .then(() => open(url)),
-  )
-  .then(() => new Promise(resolve => setTimeout(resolve, 5000)));
+    .then(() => new Promise(resolve => setTimeout(resolve, 5000)));
 
 const handleTrainingResults = (options, input, { useMeta }) => {
-  const { history: { loss, val_loss }} = input;
+  const {
+    history: { loss, val_loss }
+  } = input;
   useMeta(useMeta());
   return ensureServerLoaded()
-    .then(() => requestLine(
-      options.title || '📉  Training Results',
-      Object
-        .fromEntries(
+    .then(() =>
+      requestLine(
+        options.title || "📉  Training Results",
+        Object.fromEntries(
           [
-            (!!typeCheck('[Number]', loss)) && ['loss', { data: loss, backgroundColor: '#2d5ba6' }],
-            (!!typeCheck('[Number]', val_loss)) && ['val_loss', { data: val_loss, backgroundColor: '#a8328d' }],
-          ]
-            .filter(e => !!e),
-        ),
-    ))
+            !!typeCheck("[Number]", loss) && [
+              "loss",
+              { data: loss, backgroundColor: "#2d5ba6" }
+            ],
+            !!typeCheck("[Number]", val_loss) && [
+              "val_loss",
+              { data: val_loss, backgroundColor: "#a8328d" }
+            ]
+          ].filter(e => !!e)
+        )
+      )
+    )
     .then(() => input);
 };
 
 const handleDefault = ({ title }, input, { useMeta }) => {
   useMeta(useMeta());
   return ensureServerLoaded()
-    .then(() => requestJson(title || '🌳 Log', input))
+    .then(() => requestJson(title || "🌳 Log", input))
     .then(() => input);
 };
 
 export const viz = (options = defaultOptions) => handle => {
   const opts = {
     ...defaultOptions,
-    ...options,
+    ...options
   };
-  handle('{params:{...},epoch:[Number],history:{...},...}', (input, hooks) => handleTrainingResults(opts, input, hooks));
-  handle('*', (input, hooks) => handleDefault(opts, input, hooks));
+  handle("{params:{...},epoch:[Number],history:{...},...}", (input, hooks) =>
+    handleTrainingResults(opts, input, hooks)
+  );
+  handle("*", (input, hooks) => handleDefault(opts, input, hooks));
 };
